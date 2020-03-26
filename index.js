92a02c64bcfb5f7327c682f01f1fb9fbc8648bcf
Riccardo <Coppola>
2020-03-26 17:42:18 +0000

    Adds compose and pipe to index

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const bind = require("./bind/bind");
 const map = require("./map/map");
 const reduce = require("./reduce/reduce");
 const curry = require("./curry/curry");
+const compose = require("./compose/compose");
+const pipe = require("./pipe/pipe");
 
 module.exports = {
   bind,
   map,
   reduce,
-  curry
+  curry,
+  compose,
+  pipe
 };
